ca040e695ebea6e953e5e90f3542ec5d228b3868
vidos <vid.osep@gmail.com>
2023-06-07 17:58:20 +0200

    Many new changes

--- assets/ts4 - Osnovno rastlinje.tsx
+++ assets/ts4 - Osnovno rastlinje.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="ts4 - Osnovno rastlinje" tilewidth="16" tileheight="16" tilecount="45" columns="9">
- <image source="../../Downloads/Sprout Lands - Sprites - Basic pack/Sprout Lands - Sprites - Basic pack/Objects/Basic Grass Biom things 1.png" width="144" height="80"/>
+ <image source="Objects/Basic Grass Biom things 1.png" width="144" height="80"/>
 </tileset>
